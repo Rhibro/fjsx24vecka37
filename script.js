6197725ba6ae531632413e0c6ea26eea38724ade
Rhibro <rhibron@gmail.com>
2025-02-10 11:23:33 +0100

    function

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ document.querySelectorAll(".nav-links").forEach(n => n.
         navLinks.classList.remove("active");
     }))
 
+   
+
+    function checkAndConvert() {
+        let userInput = prompt("Please enter a value:");
+        let type = typeof userInput;
+    
+        console.log(`Original input: ${userInput}`);
+        console.log(`Type: ${type}`);
+    
+        if (type === "string") {
+            let numConversion = Number(userInput);
+            console.log(`Converted to number: ${numConversion}`);
+            console.log(`Is NaN: ${isNaN(numConversion)}`);
+        } else if (type === "number") {
+            console.log(`Converted to string: ${userInput.toString()}`);
+        } else if (type === "boolean") {
+            console.log(`Converted to string: ${userInput.toString()}`);
+            console.log(`Converted to number: ${Number(userInput)}`);
+        } else {
+            console.log("The input is neither a string, number, nor boolean.");
+        }
+    }
+    
+    checkAndConvert();
